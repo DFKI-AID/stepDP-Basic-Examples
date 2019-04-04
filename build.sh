6951ab1f9963eabe1c02a442d8fc90983664154d
Yannick Körber <yako02@dfki.de>
2019-04-04 13:57:58 +0200

    removed old build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-mvn clean install -f external/step-dp/src/pom.xml -am -pl spring
-mvn package
-java -Dde.dfki.step.dialog.MyDialog10 -jar target/mydialog.jar
-
-
-echo "Finished. Press enter to coninue."
-read
