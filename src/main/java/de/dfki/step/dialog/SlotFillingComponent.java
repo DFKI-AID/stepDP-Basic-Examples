c9b056ccf6da051e2dbf4751c271996b69507298
Magdalena Kaiser <maka04@employee-dynamic-63-205.sb.dfki.de>
2019-05-14 15:54:16 +0200

    taskrule example

---
+++ src/main/java/de/dfki/step/dialog/SlotFillingComponent.java
@@ -0,0 +1,60 @@
+package de.dfki.step.dialog;
+
+import de.dfki.step.core.Component;
+import de.dfki.step.core.ComponentManager;
+import de.dfki.step.core.Token;
+import de.dfki.step.core.TokenComponent;
+import de.dfki.step.kb.AttributeValue;
+import de.dfki.step.kb.Entity;
+import de.dfki.step.output.PresentationComponent;
+import de.dfki.step.rengine.CoordinationComponent;
+import de.dfki.step.rengine.RuleSystemComponent;
+
+
+
+public class SlotFillingComponent implements Component {
+    private RuleSystemComponent rsc;
+    private TokenComponent tc;
+    private PresentationComponent pc;
+    private Entity frameInfo;
+
+    SlotFillingComponent(Entity frameInfo) {
+        this.frameInfo = frameInfo;
+    }
+
+    @Override
+    public void init(ComponentManager cm) {
+        rsc = cm.retrieveComponent(RuleSystemComponent.class);
+        tc = cm.retrieveComponent(TokenComponent.class);
+        pc = cm.retrieveComponent(PresentationComponent.class);
+
+    }
+
+    @Override
+    public void deinit() {
+
+    }
+
+    @Override
+    public void update() {
+        for(String key: frameInfo.attributes.keySet()) {
+            for(Token token: tc.getTokens()) {
+                if(token.has(key)) {
+                    //frameInfo.attributes.get(key) = new AttributeValue(key, token.get(key).get(), frameInfo.);
+                }
+            }
+
+        }
+
+    }
+
+    @Override
+    public Object createSnapshot() {
+        return null;
+    }
+
+    @Override
+    public void loadSnapshot(Object snapshot) {
+
+    }
+}
