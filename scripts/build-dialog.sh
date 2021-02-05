edb2c35ade6d6ad7e9a828e6ce1e39ca63e172a4
adpo01 <adpo01@dfki.de>
2021-02-05 09:18:53 +0100

    delete old examples

--- scripts/build-dialog.sh
+++
@@ -1,5 +0,0 @@
-mvn clean install -f ../external/step-dp/src/pom.xml -am -pl spring
-mvn clean package -f ../pom.xml
-
-echo "Finished. Press enter to coninue."
-read
--- scripts/run-dialog.sh
+++
@@ -1,4 +0,0 @@
-# TODO settings for log into file
-java -Dde.dfki.step.dialog.MyDialog -jar ../target/mydialog.jar
-echo "Finished. Press enter to coninue."
-read
